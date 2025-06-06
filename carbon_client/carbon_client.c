de8f7ca9025a2191387aae0a471f20d6c618c06e
rxrnokd <gooteagong@gmail.com>
2025-06-06 16:16:04 +0900

    Sixth commit
    주요 기능 변경 및 코드 정리
    
    - `userdat` 구조체에서 `bus`와 `subway` 필드를 제거하고 `gas` 필드를 추가했습니다.
    - `carbon_emissions_warning` 함수에 새로운 경고 메시지를 추가했습니다.
    - `Total_carbon_emissions` 함수에서 `bus`와 `subway`의 배출량 계산을 제거하고 `gas`의 배출량 계산을 추가했습니다.
    - `today_emmission` 함수에서 `bus` 입력 요청을 `gas`로 변경했습니다.
    - 주간 통계를 시각적으로 표현하기 위한 `draw_bar`, `get_day`, `graph_by_weekday` 함수를 새로 추가했습니다.
    - `cumulative_statistics` 함수에서 주간 통계를 출력하는 기능을 추가했습니다.
    - `first_window` 함수에서 불필요한 주석을 제거했습니다.

--- carbon_client/carbon_client.c
+++ carbon_client/carbon_client.c
@@ -6,6 +6,7 @@
 #include <winsock2.h>
 #include <windows.h>
 #include <time.h>
+#include <conio.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
@@ -16,8 +17,7 @@ typedef struct {
 
 typedef struct {
     int car;
-    int bus;
-    int subway;
+    int gas;
     int elec;
     int egg;
     int disposable;
@@ -54,15 +54,32 @@ int main() {
 
 void carbon_emissions_warning(userdat* data)
 {
-
-    if (data->total > 1000)
+    if (data->car > 20)
     {
-        printf("경고: 탄소 배출량이 1000kg을 초과했습니다!\n");
+        printf("경고: 자동차 이동거리가 20km를 초과했습니다! 대중교통을 이용해주세요\n");
+    }
+    if (data->gas > 10)
+    {
+        printf("경고: 가스 사용량이 10m^3을 초과했습니다! 보일러를 절약해주세요\n");
+    }
+
+    if (data->elec > 30)
+    {
+        printf("경고: 전기 사용량이 30kWh을 초과했습니다! 불필요한 전기 차단 해주세요\n");
+    }
+
+    if (data->disposable > 5)
+    {
+        printf("경고: 일회용품 사용 개수가 5개를 초과했습니다! 텀블러, 장바구니 들고 다녀 주세요\n");
+    }
+    if (data->total > 500)
+    {
+        printf("경고: 탄소 배출량이 500kg을 초과했습니다!\n");
         printf("\n");
     }
-    else if (data->total > 500)
+    else if (data->total > 250)
     {
-        printf("주의: 탄소 배출량이 500kg을 초과했습니다!\n");
+        printf("주의: 탄소 배출량이 250kg을 초과했습니다!\n");
         printf("\n");
     }
 }
@@ -73,14 +90,13 @@ void Total_carbon_emissions(userdat* data)
     double elec_emission = 0.424 * data->elec;
     double disposable_emission = 0.1 * data->disposable;
     double egg_emission = 0.3 * data->egg;
-    double bus_emission = 0.08 * data->bus;
-    double subway_emission = 0.04 * data->subway;
+    double gas_emission = 0.08 * data->gas;
+ 
 
-    data->total = car_emission + elec_emission + disposable_emission + egg_emission + bus_emission + subway_emission;
+    data->total = car_emission + elec_emission + disposable_emission + egg_emission + gas_emission;
 
     printf("자동차: %d km x 0.2 = %.2f kg\n", data->car, car_emission);
-    printf("버스: %d km x 0.08 = %.2f kg\n", data->bus, bus_emission);
-    printf("지하철: %d km x 0.04 = %.2f kg\n", data->subway, subway_emission);
+    printf("가스: %d m^3 x 0.08 = %.2f kg\n", data->gas, gas_emission);
     printf("전기: %d kWh x 0.424 = %.2f kg\n", data->elec, elec_emission);
     printf("일회용품: %d 개 x 0.1 = %.2f kg\n", data->disposable, disposable_emission);
     printf("계란: %d 개 x 0.3 = %.2f kg\n", data->egg, egg_emission);
@@ -108,30 +124,17 @@ void today_emmission(userdat* data, const char* id, SOCKET sock)
     data->car = temp;
 
     do {
-        printf("오늘 버스 이동거리(km): ");
+        printf("오늘 가스 사용량(m^3): ");
         if (scanf_s("%d", &temp) != 1) {
             while (getchar() != '\n');
             printf("잘못된 입력입니다. 숫자를 입력해주세요.\n");
             continue;
         }
         if (temp < 0) {
-            printf("이동거리는 0 이상이어야 합니다.\n");
+            printf("사용량은 0 이상이어야 합니다.\n");
         }
     } while (temp < 0);
-    data->bus = temp;
-
-    do {
-        printf("오늘 지하철 이동거리(km): ");
-        if (scanf_s("%d", &temp) != 1) {
-            while (getchar() != '\n');
-            printf("잘못된 입력입니다. 숫자를 입력해주세요.\n");
-            continue;
-        }
-        if (temp < 0) {
-            printf("이동거리는 0 이상이어야 합니다.\n");
-        }
-    } while (temp < 0);
-    data->subway = temp;
+    data->gas = temp;
 
     do {
         printf("오늘 전기 사용량(kWh): ");
@@ -181,6 +184,51 @@ void today_emmission(userdat* data, const char* id, SOCKET sock)
 	
 }
 
+void draw_bar(int value, int max_value, int width)
+{
+    int bar_length = (int)((double)value / max_value * width);
+    for (int i = 0; i < bar_length; i++) {
+        printf("#");
+    }
+    for (int i = bar_length; i < width; i++) {
+        printf(" ");
+    }
+}
+
+char* get_day(int day)
+{
+    switch (day) {
+    case 0: return "일요일";
+    case 1: return "월요일";
+    case 2: return "화요일";
+    case 3: return "수요일";
+    case 4: return "목요일";
+    case 5: return "금요일";
+    case 6: return "토요일";
+    default: return "알 수 없음";
+    }
+}
+
+void graph_by_weekday(userdat* data, int cnt)
+{
+    double weekday_total[7] = { 0 };
+    double max_total = 0;
+
+    for (int i = 0; i < cnt; i++) {
+        int wday = data[i].today.tm_wday; 
+        weekday_total[wday] = data[i].total;
+        if (weekday_total[wday] > max_total)
+            max_total = weekday_total[wday];
+    }
+
+    printf("\n[요일별 탄소 배출량 그래프]\n");
+    for (int i = 0; i < 7; i++) {
+        printf("%s: ", get_day(i));
+        draw_bar((int)weekday_total[i], (int)max_total == 0 ? 1 : (int)max_total, 50);
+        printf(" %.1f kg CO2 \n", weekday_total[i]);
+    }
+}
+
 void cumulative_statistics(const char* id, SOCKET sock)
 {
     system("cls");
@@ -223,6 +271,8 @@ void cumulative_statistics(const char* id, SOCKET sock)
     printf("일평균 탄소 배출량: %.1f kg CO2\n", sum / cnt);
     printf("지금까지의 하루 최대 배출량: %.1f kg CO2 %d년 %d월 %d일\n", data[maxi].total, data[maxi].today.tm_year, data[maxi].today.tm_mon, data[maxi].today.tm_mday);
     printf("지금까지의 하루 최소 배출량: %.1f kg CO2 %d년 %d월 %d일\n", data[mini].total, data[mini].today.tm_year, data[mini].today.tm_mon, data[mini].today.tm_mday);
+
+    graph_by_weekday(data, cnt);
 
     free(data);
 }
@@ -330,8 +380,8 @@ void first_window(userdat* data, userinf* user, SOCKET sock)
             }
             else if (sf == 'f')
             {
-				continue;
-                /*
+				
+                
                 printf("로그인 실패\n");
                 printf("\n");
                 printf("엔터 키를 누르면 메인 메뉴로 돌아갑니다...\n");
@@ -342,7 +392,7 @@ void first_window(userdat* data, userinf* user, SOCKET sock)
                 {
                     continue;
                 }
-                */
+                
             }
         }
         else if (op == '2')
